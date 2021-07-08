df8dcb1f3f273517a46574f6a10365924d3bd50a
Kelvin Hernandez <youmater2001@gmail.com>
2021-07-08 12:05:15 -0400

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LosAguacates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
